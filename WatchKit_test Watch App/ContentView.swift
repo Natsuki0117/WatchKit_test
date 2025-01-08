36334beee8554ee26d58a0a20fcd54f9f8add3e8
Natsuki0117 <natsu000117@icloud.com>
2025-01-08 23:47:22 +0900

    Initial Commit

---
+++ WatchKit_test Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatchKit_test Watch App
+//
+//  Created by 金井菜津希 on 2025/01/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WatchKit_test Watch App/WatchKit_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchKit_testApp.swift
+//  WatchKit_test Watch App
+//
+//  Created by 金井菜津希 on 2025/01/08.
+//
+
+import SwiftUI
+
+@main
+struct WatchKit_test_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
